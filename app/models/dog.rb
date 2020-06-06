f919ba4edf2ab90f31a625fa45883233efbeb0ac
Jack Madden <jck13mad@gmail.com>
2020-06-06 15:48:22 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,13 @@
+class Dog < Sinatra::Base 
+  
+  attr_accessor :name, :breed, :age
+  
+  def initialize(name, breed, age)
+    @name = name,
+    @breed = breed,
+    @age = age
+  end
+    
+  
+  
+end
