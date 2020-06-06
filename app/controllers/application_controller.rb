3007c0f80e9afe70f68b616e08d383eda2e0ce8c
Jack Madden <jck13mad@gmail.com>
2020-06-06 15:46:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,3 +9,17 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 end
+
+class Dog < Sinatra::Base 
+  
+  attr_accessor :name, :breed, :age
+  
+  def initialize(name, breed, age)
+    @name = name,
+    @breed = breed,
+    @age = age
+  end
+    
+  
+  
+end
